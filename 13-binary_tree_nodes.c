46645c7a8bc699484f3f0604e5cf30f28a3bb183
Meyvel22 <meyvel22@gmail.com>
2022-09-21 11:42:42 -0700

    13. Nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - unction that checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0. If node is NULL, return 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ * Return: If tree is NULL, the function must return 0
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count_l;
+	size_t count_r;
+
+	if (tree == NULL || binary_tree_is_leaf(tree) == 1)
+		return (0);
+
+	count_l = binary_tree_nodes(tree->left);
+	count_r = binary_tree_nodes(tree->right);
+
+	return (count_l + count_r + 1);
+}
